e71571315207011399c8428a6a068dab6f87c4ec
kelvinmurimi <murimikelvin229@gmail.com>
2024-10-14 18:13:57 +0300

    fix careers page add banner

--- resources/views/frontend/pages/careers.blade.php
+++ resources/views/frontend/pages/careers.blade.php
@@ -1,5 +1,6 @@
 @extends('frontend.base')
 @section('content')
+@include('frontend.includes.banner')
 <div class="alert alert-success">
     please visit later this page is  currently under maintainance.
 </div>
