c47c0b139a30b57cc0598951b018e3d3e832466b
kelvinmurimi <murimikelvin229@gmail.com>
2024-10-14 17:25:30 +0300

    for push

--- resources/views/frontend/includes/Counter.blade.php
+++ resources/views/frontend/includes/Counter.blade.php
@@ -6,8 +6,8 @@
                 <div class="facts-item">
                     <i class="flaticon-home"></i>
                     <div class="facts-text">
-                        <h3 class="facts-plus" data-toggle="counter-up">150</h3>
-                        <p>Countries</p>
+                        <h3 class="facts-plus" data-toggle="counter-up">1500</h3>
+                        <p>children</p>
                     </div>
                 </div>
             </div>
@@ -15,7 +15,7 @@
                 <div class="facts-item">
                     <i class="flaticon-charity"></i>
                     <div class="facts-text">
-                        <h3 class="facts-plus" data-toggle="counter-up">400</h3>
+                        <h3 class="facts-plus" data-toggle="counter-up">300</h3>
                         <p>Volunteers</p>
                     </div>
                 </div>
@@ -24,17 +24,17 @@
                 <div class="facts-item">
                     <i class="flaticon-kindness"></i>
                     <div class="facts-text">
-                        <h3 class="facts-dollar" data-toggle="counter-up">10000</h3>
-                        <p>Our Goal</p>
+                        <h3 class="facts-plus" data-toggle="counter-up">10</h3>
+                        <p>partners</p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
                 <div class="facts-item">
-                    <i class="flaticon-donation"></i>
+                    <i class="flaticon-award"></i>
                     <div class="facts-text">
-                        <h3 class="facts-dollar" data-toggle="counter-up">5000</h3>
-                        <p>Raised</p>
+                        <h3 class="facts-plus" data-toggle="counter-up">20</h3>
+                        <p>Awards</p>
                     </div>
                 </div>
             </div>
--- resources/views/frontend/pages/donation.blade.php
+++ resources/views/frontend/pages/donation.blade.php
@@ -14,5 +14,13 @@
         </div>
     </div>
 @endif
+  <h3 class="text-primary">Than you for choosing to touch childrens life .
+    follow donate button below to proceed to donation methods.
+  </h3>
+<form action="https://www.paypal.com/donate" method="post" target="_top">
+    <input type="hidden" name="hosted_button_id" value="LU7GDTJZH7NLG" />
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    <img alt="" border="0" src="https://www.paypal.com/en_KE/i/scr/pixel.gif" width="1" height="1" />
+    </form>
     @include('frontend.includes.BecomeVolunteer')
 @endsection
