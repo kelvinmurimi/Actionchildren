285cbd413f37178683be3a7b9069dffa352dd88a
kelvinmurimi <murimikelvin229@gmail.com>
2024-10-15 09:45:19 +0300

    add widget

--- resources/views/frontend/base.blade.php
+++ resources/views/frontend/base.blade.php
@@ -36,7 +36,19 @@
             }
         </style>
     </head>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/62874b747b967b1179905840/1g3g7tgau';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
     <body>
         <!-- Top Bar Start -->
         <div class="top-bar d-none d-md-block">
