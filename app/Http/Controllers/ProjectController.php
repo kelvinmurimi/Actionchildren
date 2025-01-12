8c00d625c635e37b80317adb6d80d6dcf1235372
kelvinmurimi <murimikelvin229@gmail.com>
2025-01-12 08:12:04 +0300

    [Feature]Add Project Resources

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Project $project)
+    {
+        //
+    }
+
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        //
+    }
+
+    public function destroy(Project $project)
+    {
+        //
+    }
+}
---
+++ app/Policies/ProjectPolicy.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProjectPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    public function view(User $user, Project $project)
+    {
+        //
+    }
+
+    public function create(User $user)
+    {
+        //
+    }
+
+    public function update(User $user, Project $project)
+    {
+        //
+    }
+
+    public function delete(User $user, Project $project)
+    {
+        //
+    }
+
+    public function restore(User $user, Project $project)
+    {
+        //
+    }
+
+    public function forceDelete(User $user, Project $project)
+    {
+        //
+    }
+}
