dd795739364ff573794ac17e7a57c71a9b6c4b97
kelvinmurimi <murimikelvin229@gmail.com>
2025-01-14 07:09:27 +0300

    add fix to logo

--- resources/views/frontend/base.blade.php
+++ resources/views/frontend/base.blade.php
@@ -171,7 +171,9 @@
     <!-- Nav Bar Start -->
     <div class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
-            <a href="/" class="navbar-brand">AFCIC</a>
+            <a href="/" class="navbar-brand">
+            <img src="{{ asset('logo.png') }}" alt="Afcic logo" >
+            </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
