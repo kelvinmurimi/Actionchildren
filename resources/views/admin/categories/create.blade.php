72eee62070e2b0e92a162f5cbde217a64f8da802
kelvin kinyua <73465239+kelvinmurimi@users.noreply.github.com>
2025-02-02 16:50:35 +0300

    [Feature] Refactor categories to pages

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -4,7 +4,7 @@
         <div class="card">
 
             <div class="flex justify-between">
-                <h4 class="mb-5 text-bold text-indigo-800">{{ __('Publish Category') }}</h4>
+                <h4 class="mb-5 text-bold text-indigo-800">{{ __('Publish Page') }}</h4>
 
                 <div>
 
@@ -12,7 +12,7 @@
                     <span class="dark:text-gray-200"> = {{ __('required') }}</span>
                 </div>
                 <div class="flex space-x-2 justify-end p-3">
-                    <a href="{{ route('categories.index') }}"class="btn btn-primary p-3">{{ __('Back To Categories') }}</a>
+                    <a href="{{ route('categories.index') }}"class="btn btn-primary p-3">{{ __('Back To Pages') }}</a>
 
                 </div>
             </div>
@@ -22,7 +22,7 @@
                 <div class="mb-5">
                     <div class="rounded-md shadow-sm">
                         {{-- Article Title --}}
-                        <label for="Article Title" class="block text-sm font-medium leading-5 text-gray-700 dark:text-gray-200"><strong>Category Name</strong></label>
+                        <label for="Article Title" class="block text-sm font-medium leading-5 text-gray-700 dark:text-gray-200"><strong>Page Name</strong></label>
                         <input type="text" name='name'
                             class="block w-full dark:bg-gray-500 dark:text-gray-200 dark:placeholder-gray-200 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-light-blue-500 focus:border-light-blue-500 sm:text-sm"
                             value="  {{ old('name') }}">
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,7 +3,7 @@
 <div>
     <div class="flex justify-between">
 
-        <h1>{{ __(' Categories') }}</h1>
+        <h1>{{ __('  Resource Pages') }}</h1>
 
         <div>
 
@@ -14,21 +14,21 @@
     <div class="mt-5 mb-5 grid sm:grid-cols-1 md:grid-cols-3 gap-4">
 
         <div class="col-span-2">
-            <x-form.input type="search" name="name" wire:model="name" label="none" :placeholder="__('Search Articles')" />
+            <x-form.input type="search" name="name" wire:model="name" label="none" :placeholder="__('Search Pages')" />
         </div>
 
     </div>
 
     <div class="mb-5">
 
-        <button type="button" @click="isOpen = !isOpen" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded-t text-grey-700 bg-gray-200 hover:bg-grey-300 dark:bg-gray-700 dark:text-gray-200 transition ease-in-out duration-150">
+        <button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded-t text-grey-700 bg-gray-200 hover:bg-grey-300 dark:bg-gray-700 dark:text-gray-200 transition ease-in-out duration-150">
             <svg class="h-5 w-5 text-gray-500 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
             </svg>
             {{ __('Advanced Search') }}
         </button>
 
-        <button type="button" wire:click="resetFilters" @click="isOpen = false" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-grey-700 bg-gray-200 hover:bg-grey-300 dark:bg-gray-700 dark:text-gray-200 transition ease-in-out duration-150">
+        <button type="button" wire:click="resetFilters" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded text-grey-700 bg-gray-200 hover:bg-grey-300 dark:bg-gray-700 dark:text-gray-200 transition ease-in-out duration-150">
             <svg class="h-5 w-5 text-gray-500 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
             </svg>
@@ -54,7 +54,7 @@
     </div>
 
     <div class="flex space-x-2 justify-end p-3">
-        <a href="{{ route('categories.create') }}"class="btn btn-primary p-3" >{{ __('Add Category') }}</a>
+        <a href="{{ route('categories.create') }}"class="btn btn-primary p-3" >{{ __('Add Page') }}</a>
 
      </div>
       @include('errors.messages')
@@ -63,7 +63,7 @@
         <thead>
         <tr>
 
-            <th><a href="#" wire:click.prevent="sortBy('first_name')">{{ __('Category Name') }}</a></th>
+            <th><a href="#" wire:click.prevent="sortBy('first_name')">{{ __('Page Name') }}</a></th>
             <th><a href="#" wire:click.prevent="sortBy('email')">{{ __('Action') }}</a></th>
 
         </tr>
